2eac718794d6ef31c9319f23041810dcef46d19a
c1322167 Rodrigo Santana Almeida <c1322167@interno.bb.com.br>
2022-01-07 17:48:53 -0300

    Force de teste FAIL para captura d Jenkins-Sonar

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -11,7 +11,7 @@ public class DateUtilsTest {
 	@Test
 	public void deveRetornarTrueParaDatasFuturas() {
 		LocalDate date = LocalDate.of(2030, 1, 1);
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
 	}
 	
 	@Test
